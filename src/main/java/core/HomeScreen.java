b4bccc66e166a5ecd239c5bf9a54b482a2bbb548
Eucrite <yangtzb@gmail.com>
2018-09-09 23:19:53 -0400

    main: add cards animation and click handler

--- src/main/java/core/HomeScreen.java
+++ src/main/java/core/HomeScreen.java
@@ -1,5 +1,19 @@
 package core;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.util.Random;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -8,20 +22,30 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class HomeScreen extends Application{
 
+	private static final Logger logger = LogManager.getLogger(HomeScreen.class);
+	
 	private Button solnBtn;
 	private TextField leftOperandTxtBox;
 	private TextField rightOperandTxtBox;
 	private TextField answerTxtBox;
 	private ComboBox<String> operatorDropdown;
+	private Image[] cardsImg;
+	private ImageView imgView;
 	
 	public static void main(String[] args) {
+		logger.info("Home Screen booting up ...");
+
 		launch(args);
 	}
 	
@@ -35,13 +59,70 @@ public class HomeScreen extends Application{
 		//canvas.setStyle("-fx-background-color: black");
 		
 		addControlsToCanvas(canvas);
+		setupCardsAnimation(canvas);
 			
-		Scene scene = new Scene(canvas, 320, 200);
+		Scene scene = new Scene(canvas, 420, 400);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Simple Calculator App");
 		primaryStage.show();
 	}
 	
+	private void setupCardsAnimation(Pane canvas) {
+		File cardsDir = new File("src/main/resources/core/cards");
+		FilenameFilter imgFilter = new FilenameFilter() {
+			
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.toLowerCase().endsWith("jpg");
+			}
+		};
+		
+		File[] cardsFile = cardsDir.listFiles(imgFilter);
+		cardsImg = new Image[cardsFile.length];
+		int idx = 0;
+		
+		for(File cardFile : cardsFile) {
+			try {
+				cardsImg[idx] = new Image(new FileInputStream(cardFile.getPath()));
+				idx++;
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+		}
+
+		imgView = new ImageView();
+		imgView.setImage(cardsImg[0]);
+		imgView.relocate(20, 120);
+		imgView.setFitWidth(150);
+		imgView.setFitHeight(200);
+		imgView.setPreserveRatio(true);
+		
+		Timeline timeLine = new Timeline();
+		timeLine.setAutoReverse(true);
+		timeLine.setCycleCount(Animation.INDEFINITE);
+		
+		KeyValue keyValue = new KeyValue(imgView.xProperty(), 200, Interpolator.EASE_BOTH);
+		KeyFrame keyFrame = new KeyFrame(Duration.millis(800), keyValue);
+		
+		timeLine.getKeyFrames().add(keyFrame);
+		timeLine.play();
+		
+		setCardClickHandler();
+		
+		canvas.getChildren().addAll(imgView);
+	}
+
+	private void setCardClickHandler() {
+		Random rand = new Random();
+		
+		imgView.addEventHandler(MouseEvent.MOUSE_CLICKED, event ->{
+			logger.info("card click detected");
+			Image randomImage = cardsImg[rand.nextInt(cardsImg.length)];
+			imgView.setImage(randomImage);
+		});
+	}
+
 	private void addControlsToCanvas(Pane canvas) {
 		int row1 = 20;
 		int row2 = 60;
@@ -78,10 +159,10 @@ public class HomeScreen extends Application{
 	}
 
 	private void setSolnBtnClickHandler() {
-		// TODO Auto-generated method stub
 		solnBtn.setOnAction(new EventHandler<ActionEvent>() {
 			
 			public void handle(ActionEvent event) {
+				logger.info("Solving Button clicked");
 				double leftOperand = Double.valueOf(leftOperandTxtBox.getText());
 				double rightOperand = Double.valueOf(rightOperandTxtBox.getText());
 				String operator = operatorDropdown.getValue();
